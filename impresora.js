3a7f1f2a6e27e70bd505b976fb0ba34c1adad192
JessMars <126720608+JessMars@users.noreply.github.com>
2023-03-05 21:03:54 -0300

    Add files via upload

---
+++ impresora.js
@@ -0,0 +1,11 @@
+var btn = document.querySelector("button");
+btn.onclick = () =>{
+    window.print()
+    
+}
+
+var btn = document.querySelector("Aptitudes");
+btn.onclick = () =>{
+    window.print()
+    
+}
